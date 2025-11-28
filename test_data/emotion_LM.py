309cd5e4505d78c357c98b53dbd54b0ed8b27c06
SamuelHellqvist <samuel@hellqvist.nu>
2025-11-28 13:27:43 +0100

    Update emotion_LM.py
    lets get the emotions from the descriptions

--- test_data/emotion_LM.py
+++ test_data/emotion_LM.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+import pandas as pd
 
 # Load the model and request full score distribution
 classifier = pipeline(
@@ -6,6 +7,17 @@ classifier = pipeline(
     model="j-hartmann/emotion-english-distilroberta-base",
     return_all_scores=True
 )
+
+# read the descriptions from the data file
+usecols = ["Title", "Description"]
+
+# Read the CSV file
+df = pd.read_csv("test_data/16k_Movies.csv", usecols=usecols)
+
+desc_texts = df['Description'].tolist()
+
+print(desc_texts[1])
+
 
 text = (
     "Ed and Lorraine Warren, world-renowned investigators of supernatural events, are called in to help a family terrorized by dark forces. In the family’s house, deep in the countryside, the Warrens are forced to confront a powerful demonic presence."
